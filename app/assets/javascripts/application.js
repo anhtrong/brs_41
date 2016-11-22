ee2825aa3c7ac259153b6d56a6448b38e7487535
ANH TRONG <anhtrong993@gmail.com>
2016-11-22 17:16:04 +0700

    Static pages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,10 @@
-// This is a manifest file that"ll be compiled into application.js, which will include all the files
+// This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin"s vendor/assets/javascripts directory can be referenced here using a relative path.
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
 //
-// It"s not advisable to add code directly here, but if you do, it"ll appear at the bottom of the
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file. JavaScript code in this file should be added after the last require_* statement.
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
@@ -13,4 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require jquery-1.10.2
+//= require jquery-migrate-1.2.1
 //= require_tree .
